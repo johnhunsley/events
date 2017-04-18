716add9e0b5f11343840332441468391a8d3d5b8
johnhunsley <snowball33>
2017-04-18 10:35:30 +0100

    set tomcat system prop to allow encoded forward slashes

--- src/main/java/com/johnhunsley/events/Application.java
+++ src/main/java/com/johnhunsley/events/Application.java
@@ -2,7 +2,9 @@ package com.johnhunsley.events;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.util.UrlPathHelper;
 
 /**
  * @author John Hunsley
@@ -10,9 +12,17 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
  *         Date : 06/03/2017
  */
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) throws Exception {
+        System.setProperty("org.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH", "true");
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        UrlPathHelper urlPathHelper = new UrlPathHelper();
+        urlPathHelper.setUrlDecode(false);
+        configurer.setUrlPathHelper(urlPathHelper);
     }
 }
