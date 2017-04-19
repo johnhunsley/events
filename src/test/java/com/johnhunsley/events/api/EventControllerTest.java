506e77a6caf75466b00950bcf931d8f97affe100
johnhunsley <snowball33>
2017-04-19 10:52:38 +0100

    integration test & config

--- src/test/java/com/johnhunsley/events/api/EventControllerTest.java
+++ src/test/java/com/johnhunsley/events/api/EventControllerTest.java
@@ -11,41 +11,30 @@ import org.joda.time.DateTime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.anyObject;
-import static org.mockito.Matchers.anyString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author John Hunsley
  *         jphunsley@gmail.com
- *         Date : 07/03/2017
- *         Time : 14:15
+ *         Date : 19/04/2017
+ *         Time : 10:14
  */
 @RunWith(SpringRunner.class)
-@WebMvcTest(value = EventsController.class, secure = false)
-public class EventsControllerTest {
+@WebMvcTest(value = EventController.class, secure = false)
+public class EventControllerTest {
 
     @MockBean
     private EventsPagingAndSortingRepository eventsRepository;
@@ -60,29 +49,37 @@ public class EventsControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    public void testGetOpenEventsByOrg() throws Exception {
+    public void testCreateEvent() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
         Date now = DateTime.now().toDate();
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd,HH:mm:ss:SSS");
-        final String expected = "{\"content\":[{\"user\":\"987654321cba\",\"created\":\""+df.format(now)+"\",\"longitude\":0.0,\"latitude\":0.0,\"priority\":\"High\",\"status\":\"Open\",\"hash\":\"pQPMBLoSO+iv+/i/hxzwjg==\",\"org\":\"4xnzkhxFnF1vMnj5N6knT4\"}],\"totalPages\":1,\"totalElements\":1,\"last\":true,\"size\":20,\"number\":0,\"sort\":null,\"numberOfElements\":1,\"first\":true}";
         Event event = new Event("987654321cba", now, "pQPMBLoSO+iv+/i/hxzwjg==", "4xnzkhxFnF1vMnj5N6knT4");
         event.setLongitude(0);
         event.setLatitude(0);
         event.setStatus("Open");
         event.setPriority("High");
-        List<Event> eventList = new ArrayList<>();
-        eventList.add(event);
-        Page<Event> page = new PageImpl<>(eventList, new PageRequest(0,20), 1);
-        given(eventFactory.resolveOrgId(anyObject())).willReturn("");
-        given(eventsRepository.findByOrgAndStatus(anyString(), anyString(), anyObject())).willReturn(page);
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("page", "0");
-        params.add("size", "20");
+        given(eventFactory.createEvent(anyObject())).willReturn(event);
+        given(eventsRepository.save(event)).willReturn(event);
 
-        mockMvc.perform(get("/app/events/").params(params))
+        mockMvc.perform(post("/app/event/").contentType("application/json").content(mapper.writeValueAsString(event)))
                 .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().json(expected))
+                .andExpect(status().is(HttpStatus.SC_ACCEPTED))
                 .andReturn();
     }
 
+    @Test
+    public void testUpdateEvent() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        Date now = DateTime.now().toDate();
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd,HH:mm:ss:SSS");
+        Event event = new Event("987654321cba", now, "pQPMBLoSO+iv+/i/hxzwjg==", "4xnzkhxFnF1vMnj5N6knT4");
+        event.setLongitude(0);
+        event.setLatitude(0);
+        event.setStatus("Open");
+        event.setPriority("High");
+        given(eventsRepository.findOne(anyObject())).willReturn(event);
+        Event update = (Event) BeanUtils.cloneBean(event);
+        update.setStatus("Closed");
+        given(eventsRepository.save(event)).willReturn(event);
+    }
 }
